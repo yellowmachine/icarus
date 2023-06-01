dde9a9ac24343f596326e3a3473f44dbf84d5b30
Yellow Machine <yellow.machine@mailfence.com>
2023-06-01 08:33:15 +0200

    npx degit kit template; waiting... message and rest of actions

--- app/src/lib/Actions.svelte
+++ app/src/lib/Actions.svelte
@@ -39,33 +39,33 @@
 
 {#if loading}
     <div class="text-orange-500">Waiting...</div>
-{:else}
-    <div class="grid grid-rows-1 grid-flow-col gap-4">
-        {#if data.isValid}
-            <button on:click={up} class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-                Up
-            </button>
-        {:else}
-            <div class="text-red font-bold">Errors in config file</div>
-        {/if}
-        
-        {#if data.services.length > 0}
-            <button on:click={down} class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded">
-                Down
-            </button>
-        {/if}
-        
-        {#if data.services.length === 0}
-            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" on:click={() => edit(data.workspace)}>
-                Edit
-            </button>
-        {/if}
-        
-        {#if data.services.length === 0}
-            <button on:click={delete_} class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-                Delete
-            </button>
-        {/if}
-    </div>
 {/if}
+<div class="grid grid-rows-1 grid-flow-col gap-4">
+    {#if data.isValid}
+        <button on:click={up} class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+            Up
+        </button>
+    {:else}
+        <div class="text-red font-bold">Errors in config file</div>
+    {/if}
+    
+    {#if data.services.length > 0}
+        <button on:click={down} class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded">
+            Down
+        </button>
+    {/if}
+    
+    {#if data.services.length === 0}
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" on:click={() => edit(data.workspace)}>
+            Edit
+        </button>
+    {/if}
+    
+    {#if data.services.length === 0}
+        <button on:click={delete_} class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+            Delete
+        </button>
+    {/if}
+</div>
+
 
