1ec340c86d27ba443544b706384070837d48cb15
Yellow Machine <yellow.machine@mailfence.com>
2023-06-02 05:53:08 +0200

    built with email and user for git

--- server/build.sh
+++ server/build.sh
@@ -1,2 +1,5 @@
-export SSH_KEY=$(cat ~/.ssh/id_ed25519)
-docker build . -t codenode -f Dockerfilenode 
+SSH_KEY=$(cat ~/.ssh/id_ed25519)
+USER=yellowm
+EMAIL=yellow.machine@mailfence.com
+
+docker build . -t codenode -f Dockerfilenode --build-arg EMAIL="$EMAIL" --build-arg USER="$USER" --build-arg SSH_KEY="$SSH_KEY"
