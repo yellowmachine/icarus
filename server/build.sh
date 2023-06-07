fa4e9a20b298de2f0b556ec6758b132362dc797b
Yellow Machine <yellow.machine@mailfence.com>
2023-06-07 06:57:36 +0200

    added code python and julia

--- server/build.sh
+++ server/build.sh
@@ -1,1 +1,7 @@
 docker build . -t yellowmachine/codenode -f Dockerfilenode
+docker build . -t yellowmachine/codepython -f Dockerfilepython
+docker build . -t yellowmachine/codejulia -f Dockerfilejulia
+
+docker push yellowmachine/codenode
+docker push yellowmachine/codepython
+docker push yellowmachine/codejulia
